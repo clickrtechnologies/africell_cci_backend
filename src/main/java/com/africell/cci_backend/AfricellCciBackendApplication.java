d3adb030358f97f37558694eae5cb60aeb2afa18
arshiaj16 <arshiaj1062@gmail.com>
2026-08-14 11:54:34 +0530

    Initial commit

---
+++ src/main/java/com/africell/cci_backend/AfricellCciBackendApplication.java
@@ -0,0 +1,13 @@
+package com.africell.cci_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricellCciBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfricellCciBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/africell/cci_backend/AfricellCciBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.africell.cci_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfricellCciBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
